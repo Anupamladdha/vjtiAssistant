5938eb5ac0218d6c58a6c478f5c41c0514098c06
AnkitJaiswal26 <jankitbb@gmail.com>
2021-05-16 15:42:05 +0530

    Add Bottom Navbar

--- lib/Screens/ResourceScreen/ResourceScreen.dart
+++ lib/Screens/ResourceScreen/ResourceScreen.dart
@@ -1,3 +1,4 @@
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
 import 'components/NavigationDrawerWidget.dart';
 
@@ -9,10 +10,42 @@ class ResourceScreen extends StatefulWidget {
 }
 
 class _ResourceScreenState extends State<ResourceScreen> {
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: NavigationDrawerWidget(),
+      bottomNavigationBar: BottomNavyBar(
+        selectedIndex: currentIndex,
+        onItemSelected: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        items: <BottomNavyBarItem>[
+          BottomNavyBarItem(
+            icon: Icon(Icons.search_sharp),
+            title: Text("Discover"),
+            inactiveColor: Color(0xFF707070),
+          ),
+          BottomNavyBarItem(
+            icon: Icon(Icons.bookmark),
+            title: Text("Bookmarks"),
+            inactiveColor: Color(0xFF707070),
+          ),
+          BottomNavyBarItem(
+            icon: Icon(Icons.add),
+            title: Text("Add book"),
+            inactiveColor: Color(0xFF707070),
+          ),
+          BottomNavyBarItem(
+            icon: Icon(Icons.library_books),
+            title: Text("My Library"),
+            inactiveColor: Color(0xFF707070),
+          ),
+        ],
+      ),
     );
   }
 }
