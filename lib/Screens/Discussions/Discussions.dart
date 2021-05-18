a121eedaad283a962d43dd9b57b69fe4aaaad72e
TANISH SAWANT <tanishsawant@rediffmail.com>
2021-05-18 16:58:52 +0530

    .

---
+++ lib/Screens/Discussions/Discussions.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DiscussionsScreen extends StatefulWidget {
+  @override
+  _DiscussionsScreenState createState() => _DiscussionsScreenState();
+}
+
+class _DiscussionsScreenState extends State<DiscussionsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+        "Discussions page"
+      )
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'Screens/Login/login.dart';
 import 'Screens/Signup/signup.dart';
 import 'Screens/Profile/profile.dart';
 import 'package:vjtiAssistant/Screens/ResourceScreen/ResourceScreen.dart';
-
+import './Screens/Discussions/Discussions.dart';
 void main() {
   runApp(MyApp());
 }
@@ -30,6 +30,7 @@ class MyApp extends StatelessWidget {
         '/login': (context) => LoginScreen(),
         '/signup': (context) => SignUpScreen(),
         '/resources/': (context) => ResourceScreen(),
+        '/discussions/': (context) => DiscussionsScreen(),
         // '/profile': (context) => Profile(),
       },
     );
